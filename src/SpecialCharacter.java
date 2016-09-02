544e3833f9ec2edaf47393a62e4d63c9b22762be
NehaJamwal <nehajamwal89@gmail.com>
2016-09-02 15:33:39 -0400

    Escape Sequence

---
+++ src/SpecialCharacter.java
@@ -0,0 +1,9 @@
+
+public class SpecialCharacter {
+	public static void main(String[] args) {
+		System.out.println("  **************");
+		System.out.println("*\t \t*\n*\t\t*\n*\t\t*\n*\t\t*");
+		
+		System.out.println("  **************");
+}
+} 
