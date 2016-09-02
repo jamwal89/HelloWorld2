423f231e9a758075aa89466199c57edc3efb8572
NehaJamwal <nehajamwal89@gmail.com>
2016-09-02 15:20:33 -0400

    Name Assignment

---
+++ src/Name.java
@@ -0,0 +1,12 @@
+
+public class Name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name;
+		name= "Neha Jamwal";
+				System.out.println("My name is " + name);
+
+	}
+
+}
